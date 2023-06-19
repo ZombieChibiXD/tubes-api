5b576f0d9719e18ec18f4e125fe367bcb55953eb
Anonymous <anon@ymo.us>
2023-06-19 14:09:04 +0800

    Auth scaffolding

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function login()
+    {
+        //
+    }
+
+    public function logout()
+    {
+        //
+    }
+}
