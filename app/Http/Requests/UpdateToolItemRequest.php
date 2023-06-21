8ce88eaf33b7b46f40bf3a44de41f5c1a2923008
Anonymous <anon@ymo.us>
2023-06-21 13:59:52 +0800

    Stop Extending

--- app/Http/Requests/UpdateToolItemRequest.php
+++ app/Http/Requests/UpdateToolItemRequest.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
+
 /**
  * @OA\Schema(
  *     schema="UpdateToolItemRequest",
  *     required={}
  * )
  */
-class UpdateToolItemRequest extends UpdateToolProductRequest
+class UpdateToolItemRequest extends FormRequest
 {
+    
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->route('tool_product'));
+    }
+    
     /**
      * Get the validation rules that apply to the request.
      *
